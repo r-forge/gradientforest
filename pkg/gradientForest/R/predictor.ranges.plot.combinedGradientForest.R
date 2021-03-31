e49d83f77ebb37571ba87e3ddb8b1d95dd11594c
pdyer <pdyer@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2021-03-31 05:47:40 +0000

    remove debugging code
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gradientforest@41 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/gradientForest/R/predictor.ranges.plot.combinedGradientForest.R
+++ pkg/gradientForest/R/predictor.ranges.plot.combinedGradientForest.R
@@ -11,7 +11,6 @@ function (obj, ...)
       return(out)
     }, obj = obj)
     )
-    browser()
     X_r$predictor <- ordered(X_r$predictor, levels=names(sort(-importance(obj))))
     print(bwplot(gf.name ~ value|predictor, X_r, scales=list(x=list(relation="free",cex=0.5)),
       par.strip.text=list(cex=0.6),xlab="Predictor value", as.table=T, ...))
